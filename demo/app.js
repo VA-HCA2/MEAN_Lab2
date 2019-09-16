2c1c627e028beb0b6777a064a54f1b1691124c26
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-09-16 13:41:12 -0400

    login page validation

--- demo/app.js
+++ demo/app.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var leaguesRouter = require('./routes/leagues');
+var teamsRouter = require('./routes/teams');
 var app = express();
 
 // view engine setup
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/leagues', leaguesRouter);
+app.use('/teams', teamsRouter);
 
 //catch 404 and forward to error handler
  app.use(function(req, res, next) {
